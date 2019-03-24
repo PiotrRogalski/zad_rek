959b41d9d128703b29c29a13ca2ae97d80d549f4
Piotr Rogalski <piotr5rogalski@gmail.com>
2019-03-24 20:04:15 +0100

    Added routes for API and web

---
+++ routes/api.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+// AUTH endpoints
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth',
+], function () {
+    Route::post('login', 'AuthController@login');
+    Route::post('signup', 'AuthController@signup');
+
+    Route::group(['middleware' => 'jwt'], function () {
+        Route::post('logout', 'AuthController@logout');
+        Route::post('refresh', 'AuthController@refresh');
+        Route::post('me', 'AuthController@me');
+        Route::post('payload', 'AuthController@payLoad');
+    });
+});
+
+// USER endpoints
+Route::resource('users', 'UserController');
+Route::get('users/show-auth', 'UserController@showAuth');
+
+// TASK endpoints
+Route::resource('tasks', 'TaskController');
+Route::get('task-user', 'TaskUserController@index');
+Route::get('task-user/{task_id}/users', 'TaskUserController@getUsers');
+Route::patch('task-user/{task_id}', 'TaskUserController@update');
+Route::delete('task-user/{task_id}', 'TaskUserController@destroy');
+
